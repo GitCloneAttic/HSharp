3ef00eaca0fae8f4f0fb365daae62dc7b8b191d9
GitCloneAttic <97304098+GitCloneAttic@users.noreply.github.com>
2022-11-14 15:31:27 -0700

    Add files via upload

--- basic.py
+++ basic.py
@@ -31,7 +31,22 @@ class RTError(Error):
         self.context = context
 
         def as_string(self):
-            pass
+            result.generate_traceback()
+            result  = f'{self.error_name}: {self.details}\n'
+            result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+            return result
+        
+        def generate_traceback(self):
+            result = ''
+            pos = self.pos_start
+            ctx = self.context
+
+            while ctx:
+                result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+                pos = ctx.parent_entry_pos
+                ctx = ctx.parent
+            
+            return 'Traceback (most recent call last):\n' + result
 
 #Position
 class Position:
